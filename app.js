b578c3b6e3ce80ce2546fc39820568c1a3e9342f
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-02-03 00:48:36 +0400

    no message

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express()
 
 let items = [];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -12,19 +14,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-
-    let today = new Date();
-
-    let options = {
-     weekday: "long",
-     day: "numeric",
-     month: "long",
- };
-  
- let day = today.toLocaleDateString("en-US", options);
+ 
+  let day = date();
 
   res.render("list", {
-      kindOfDay: day, newListItems: items});
+    listTitle: day, newListItems: items});
 });
 
 
@@ -37,6 +31,26 @@ app.post("/", function(req, res){
       
 });
 
+app.post("/work", function(req,res){
+ let item = req.body.newItem;
+
+ if (req.body.list === "work" ){
+    workItems.push(item); 
+    res.redirect("/work");
+ }  else {
+    items.push(item); 
+    res.redirect("/");
+ }
+  
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work list", newListItems: workItems })
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
