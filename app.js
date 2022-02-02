11052965a5d63c9fe3a3431a30154e631f06c305
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-02-02 23:51:55 +0400

    no message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express()
 
-var items = [];
+let items = [];
 
 app.set("view engine", "ejs");
 
@@ -12,24 +12,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
 
-var today = new Date();
+    let today = new Date();
 
- var options = {
+    let options = {
      weekday: "long",
      day: "numeric",
      month: "long",
  };
   
-  var day = today.toLocaleDateString("en-US", options);
+ let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-      kindOfDay: day, newListItem: item
-    });
+      kindOfDay: day, newListItems: items});
 });
 
 
 app.post("/", function(req, res){
-   var item = req.body.newItem;
+    let item = req.body.newItem;
 
    items.push(item);
 
